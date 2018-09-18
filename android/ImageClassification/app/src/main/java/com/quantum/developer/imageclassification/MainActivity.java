359f5305a2b8c2cc12f0df3d00520d1fb80d0898
Harish <harishtanu007@gmail.com>
2018-09-18 15:39:19 +0530

    Initial commit

---
+++ android/ImageClassification/app/src/main/java/com/quantum/developer/imageclassification/MainActivity.java
@@ -0,0 +1,19 @@
+package com.quantum.developer.imageclassification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+}
